55e342680406a24fc56e602f708b415a78fbdf7d
leoliveras <101269023+leoliveras@users.noreply.github.com>
2026-05-18 03:38:27 -0300

    Delete solution.m

--- solution.m
+++
@@ -1,8 +0,0 @@
-%................................................................
-function displacements = solution(GDof, prescribed, stiffness,force)
-    % function to find solution in terms of global displacements
-    activeDof=setdiff([1:GDof]', [prescribed]);
-    U=stiffness(activeDof,activeDof)\force(activeDof);
-    displacements=zeros(GDof,1);
-    displacements(activeDof)=U;
-end
